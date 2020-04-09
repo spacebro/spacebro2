819340a94f4adf456cc325f358a68db994b7e175
hugohil <hilairehugo@gmail.com>
2020-04-09 17:07:51 +0200

    Add auto-reconnection to client demo

--- demo/client1.js
+++ demo/client1.js
@@ -5,13 +5,36 @@ const WebSocket = require('ws')
 const host = '127.0.0.1'
 const port = 9375
 const name = 'client-1'
+const autoReconnect = true
 
-const ws = new WebSocket(`ws://${host}:${port}/?name=${name}`)
+let ws = null
+let reconnectTimeout = null
 
-ws.on('open', function open () {
-  ws.send('client 1')
-})
+function setup () {
+  ws = new WebSocket(`ws://${host}:${port}/?name=${name}`)
 
-ws.on('message', function incoming (data) {
-  console.log(data)
-})
+  ws.on('open', function open () {
+    clearTimeout(reconnectTimeout)
+    ws.send('client 1')
+  })
+
+  ws.on('message', function incoming (data) {
+    console.log(data)
+  })
+
+  ws.on('error', function error (error) {
+    // console.log(error)
+  })
+
+  ws.on('close', function closing (code) {
+    console.log('socket closed')
+    if (autoReconnect) {
+      reconnectTimeout = setTimeout(() => {
+        console.log('reconnecting...')
+        setup()
+      }, 500)
+    }
+  })
+}
+
+setup()
