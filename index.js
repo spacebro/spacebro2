343a7797d77f3c8faf40ee0363b94e5cce5c4bfb
hugohil <hilairehugo@gmail.com>
2020-04-09 16:10:05 +0200

    Add standard settings

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 'use strict'
 
 const WebSocket = require('ws')
+const standardSettings = require('standard-settings')
+const settings = standardSettings.getSettings()
 
-const port = 9375
+const host = settings.host || "127.0.0.1"
+const port = settings.port || 9375
 
-const wss = new WebSocket.Server({ port })
+const wss = new WebSocket.Server({
+  host,
+  port,
+  perMessageDeflate: false
+})
 
 wss.on('connection', function connection (ws) {
   ws.on('message', function incoming (message) {
